877420d5b9c4161282467b74ba3dc8203d8a621e
reeveboy <reeveboy10@gmail.com>
2022-10-21 09:49:29 +0530

    merge conflict

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -10,18 +9,6 @@ import Todo from "./Todo/Todo";
 import Restaurant from "./Restaurant/Restaurant";
 import Posts from "./Posts/Posts";
 import Pokemons from "./Pokemon/Pokemon";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './Birthday/App';
-import ReviewPage from './Review/ReviewPage';
-import Employees from './Employee/Employees'
-import Calculator from './Calculator/Calculator';
-import Todo from './Todo/Todo';
-import Restaurant from './Restaurant/Restaurant';
-import PA1 from './PA1/App';
->>>>>>> 5580aea63536c3eec4797039f046b553613493f1
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -32,12 +19,8 @@ root.render(
     {/* <Calculator /> */}
     {/* <Todo /> */}
     {/* <Restaurant /> */}
-<<<<<<< HEAD
     {/* <Posts /> */}
     <Pokemons />
-=======
-    <PA1 />
->>>>>>> 5580aea63536c3eec4797039f046b553613493f1
   </React.StrictMode>
 );
 
